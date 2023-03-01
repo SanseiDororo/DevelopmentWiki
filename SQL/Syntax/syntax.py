a4690dec783f9546964f4bfc7b87eb3a368ed684
SanseiDororo <philoscopes@gmail.com>
2023-03-01 09:18:11 +0100

    SQL syntax sections added

--- SQL/Syntax/syntax.py
+++ SQL/Syntax/syntax.py
@@ -13,7 +13,25 @@ def basic_syntax():
     st.write("")
     st.write("")
 
-    with st.expander("Alter Table Clause"):
+    with st.expander("CREATE TABLE"):
+        st.write(
+            """
+              The ALTER TABLE clause is a SQL command used to modify the structure of an existing table 
+              in a database. It allows you to add, delete or modify columns, change the data type of a 
+              column, rename a table or a column, add or drop constraints, and perform other modifications
+              to the table's structure.
+
+              Example
+              ```
+              ALTER TABLE table_name action statement
+              ALTER TABLE table_name ADD column_name data_type;
+
+              ```
+
+            """
+        )
+
+    with st.expander("ALTER TABLE"):
         st.write(
             """
               The ALTER TABLE clause is a SQL command used to modify the structure of an existing table 
@@ -32,28 +50,88 @@ def basic_syntax():
         )
         
 
-    with st.expander("Beetween Operator"):
+    with st.expander("DELETE"):
         st.write(
-            """   
-            The BETWEEN clause is a SQL operator that allows you to select values within 
-            a range of values. It is commonly used in SQL queries to filter data based on 
-            a specified range of values.
+            """
+            The DELETE clause in SQL is used to delete one or more rows from a table. 
+            It is commonly used to remove old or unwanted data from a database or to correct errors 
+            in existing data.
 
             Example:
 
             ```
-            SELECT column_name(s) 
-            FROM table_name 
-            WHERE column_name BETWEEN value1 AND value2;
+            DELETE FROM table_name WHERE condition;
+            ```
+
+            To delete table or database, we use DROP command
+
+            ```
+            DROB table table_name;
+            DROP DATABASE database_name;
             ```
 
             """
         )
 
-    with st.expander("Table Schema"):
+    with st.expander("SELECT"):
+        st.write(
+            """   
+            SELECT is a SQL statement used to retrieve data from one or more tables 
+            in a database. It is one of the most commonly used statements in SQL and is 
+            used to retrieve data for reporting, analysis, or other purposes.
+
+            Example:
+
+            ```
+            SELECT column1, column2, ... columnN FROM table_name;
+
+            ```
+
+            """
+        )
+    with st.expander("BETWEEN"):
+        st.write(
+            """   
+            CREATE TABLE is a SQL statement used to create a new table in a database. 
+            It specifies the table's name, the columns it will contain, and the data 
+            type of each column.
+
+            Example:
+
+            ```
+            CREATE TABLE table_name (
+            column1 datatype PRIMARY KEY,
+            column2 datatype [optional_parameters],
+            columnn datatype [optional_parameters],
+            );
+            ```
+
+            ```
+            CREATE TABLE users (
+            id INT PRIMARY KEY,
+            name VARCHAR(50) NOT NULL,
+            email VARCHAR(100) UNIQUE,
+            age INT
+            );
+            ```
+
+            """
+        )
+
+    with st.expander("COUNT"):
         st.write(
             """
-        
+            COUNT is a SQL aggregate function used to count the number of rows that meet 
+            a specified condition in a table. It is commonly used in SQL queries to calculate 
+            the number of records in a table or the number of records that meet certain criteria.
+
+            Example:
+
+            ```
+            SELECT COUNT(*) 
+            FROM users 
+            WHERE age >= 18;
+            ```
                 
             """
         )

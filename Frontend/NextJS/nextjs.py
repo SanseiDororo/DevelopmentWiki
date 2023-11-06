7e438e20ede6d27b70bc085e9b83a959eeab723d
SanseiDororo <philoscopes@gmail.com>
2023-11-06 10:45:28 +0100

    Indentation error solved.

--- Frontend/NextJS/nextjs.py
+++ Frontend/NextJS/nextjs.py
@@ -14,9 +14,7 @@ def nextjs():
             * File-Based Routing: Pages in Next.js are created by simply adding files to a pages directory, following a convention that maps filenames to routes.
             * Dynamic Routing: Next.js supports dynamic routing by using brackets in filenames, allowing for flexible and parameterized routes.
             * A Rich Ecosystem: It has a vibrant ecosystem with support for various plugins, tools, and extensions, making it easy to extend and customize your application.
-
             ''')
-
     st.write(''' ''')
     st.write(''' ''')
     
@@ -24,98 +22,73 @@ def nextjs():
         st.write(
             '''
               We can create next app with the prefered package manager, for exampke pnpm
-
               ```
                 pnpmx create-next-app@latest
               ```  
-
               This gives us dialoge screen with several options we can chose from.
-
             '''
             )
-
-     with st.expander("Authentification with Clerk"):
+    with st.expander("Authentification with Clerk"):
         st.write(
             '''
               Clerk is service which provides simple authentification integration.
-
               [More about Clerk]('https://clerk.com/docs')
-
               1. In order to be able to use Clerk, you need to sign in and create app
-
               ```
                 * Create App
                 * Define sign in methods (google, apple etx.)
                 * Copy environment keys in .env file of the project
               ```  
-
               2. Install Clerk inside the project
-
               ```
                 npm install @clerk/nextjs
               ```
-
               3. Wrap html object inside the app/layout.tsx with Clerk Provider
-
               ```
                 return (
                     <ClerkProvider>
                       <html lang="en">
                         <body>{children}</body>
-
                       </html>
                     </ClerkProvider>
                     )
-
               ```
               4. Create route protection policy by makin middleware.ts file inside you project folder
-
               ```
                 import { authMiddleware } from "@clerk/nextjs";
- 
+
                 // This example protects all routes including api/trpc routes
                 // Please edit this to allow other routes to be public as needed.
                 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
                 export default authMiddleware({});
-
                 export const config = {
                   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
                 };
- 
+
               ```
-
               5. Build sign-in and sign-up pages
-
               Inside app folder create (auth) folder and put sign-in and sign-up folder insied. It the 
               sign-in folder create [[...sign-in]] folder and do the same in sign-up folder [[...sign-up]].
               Put page.tsx in both folders. 
-
               SignUp page
-
               ```
                 import { SignUp } from "@clerk/nextjs";
- 
+
                 export default function Page() {
                   return <SignUp />;
                 }
               ```
-
               SignIn page
-
               ```
                 import { SignIn } from "@clerk/nextjs";
-
                 export default function Page() {
                   return <SignIn />;
                 }
-
               ```
-
             6. Create User Button in app page.tsx
-
             ```
                 import { UserButton } from "@clerk/nextjs";
- 
+
                 export default function Home() {
                   return (
                     <div>
@@ -123,17 +96,13 @@ def nextjs():
                     </div>
                   )
                 }
-
             ```
-
-
             '''
             )
         
-   
+  
     
+  
 
-   
-
-        
-        
+          
+          

1c39cd148611f1852b2215947bf7a1e2c56e1122
SanseiDororo <philoscopes@gmail.com>
2024-04-22 12:48:28 +0200

    File system folders bolded

--- Linux/Administration/administration.py
+++ Linux/Administration/administration.py
@@ -112,25 +112,25 @@ def administration():
                Below is the lis of linux system folder and their main purpose:
 
                * **/root** directory which is the starting point
-               * /bin contains binaries that must be present for the system to but and run
-               * /boot contains the kernel, boot image, initial RAM disk image. To confugure
+               * **/bin** contains binaries that must be present for the system to but and run
+               * **/boot** contains the kernel, boot image, initial RAM disk image. To confugure
                  the boot loader grub/grub.conf or menu.lst are used.
-               * /dev this folder contains device nodes. "Everything in linux is a file"
+               * **/dev** this folder contains device nodes. "Everything in linux is a file"
                  This is true for the devices as well.
-               * /etc contains all the system wide configuration. It contains collection of
+               * **/etc** contains all the system wide configuration. It contains collection of
                 shell scripts as well that start each of the services at boot time. Some examples
                 /etc/crontab file defines all automated jobs, etc/fstab is a table of storage devices
                 and their associated mount points.
-               * /home each user has his own home folder
-               * /lib this containes shared libraries used by the core system programs
-               * /media stores removable disks and usb drives
-               * /mnt mount points for removable devises
-               * /opt is used to install "optional" software 
-               * /root home directory of the root user
-               * /sbin contains system binaries
-               * /tmp is used to store temporary transitient files
-               * /usr it contains all the programms and support files used by regular users
-               * /var stores the data that is likely to be change   
+               * **/home** each user has his own home folder
+               * **/lib** this containes shared libraries used by the core system programs
+               * **/media** stores removable disks and usb drives
+               * **/mnt** mount points for removable devises
+               * **/opt** is used to install "optional" software 
+               * **/root** home directory of the root user
+               * **/sbin** contains system binaries
+               * **/tmp** is used to store temporary transitient files
+               * **/usr** it contains all the programms and support files used by regular users
+               * **/var** stores the data that is likely to be change   
 
                
             '''

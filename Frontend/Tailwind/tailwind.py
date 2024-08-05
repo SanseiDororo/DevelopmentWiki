981d8a93032302439e5fb16f2b93aac3748c64bc
SanseiDororo <philoscopes@gmail.com>
2024-08-05 17:30:39 +0200

    Tailwind section extended

--- Frontend/Tailwind/tailwind.py
+++ Frontend/Tailwind/tailwind.py
@@ -14,6 +14,32 @@ def tailwind():
         st.write(
             '''
                 Main styling declarations are stored in the globals.css file.
+
+                We have 3 main layers in tailwind:
+
+                * Base
+                * Components
+                * Utilities
+
+                Base layer allows us to target any html class. In Components layer, we can
+                define styles for specific components. In utilities layer we can create 
+                utility classes.
+
+                ```
+                #Applying style to more html elements:
+
+                h1,h2,h3,h4,h5,h6 {
+                  @apply font-bold;
+                }
+
+                Applying style to a single element:
+
+                h1 {
+                    font-size:2.5 rem
+                }
+                ```
+
+
             '''
             )
    
